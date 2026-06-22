7aa9e37225ea7ba1072e085ddc2b37934900d3a7
yldrmali <yldrmmmali.2@gmail.com>
2026-06-22 21:46:42 +0300

    Change base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === "true" ? "/me-wedding/" : "/",
+  base: process.env.GITHUB_PAGES === "true" ? "/personal-event/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
